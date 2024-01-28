24e3289cd6f604deed5a922a8ba30798ff313608
Amaan Bilwar <bilwarad@mail.uc.edu>
2024-01-28 04:09:22 -0500

    Basic ui stuff - first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='bg-gradient-to-r from-purple-500 to-pink-500 w-auto h-screen'>
+        <div className='w-full h-screen bg-[#FF8F0]'>
+              <div className=' font-mono'>
+                <h1 className='text-center'>Welcome To JollyLLB</h1>
+                <p className='text-center'>Let me help you with some legal advise😉</p>
+                <form>
+                  {/* <input type='text' placeholder='Ask me something!' className='border-2 mx-14 w-1/2 text-center border-black rounded-md p-2 m-2'/> */}
+                </form>  
+                
+              </div>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
